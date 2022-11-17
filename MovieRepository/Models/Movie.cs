c854880fbd9657806abb401fb51a0f887e85d2a2
Matti Lindroth <Matti.K.Lindroth@aktia.fi>
2022-11-17 14:25:51 +0200

    Initial framework for the movie repository app. Needs a lot of work still

---
+++ MovieRepository/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace MovieRepository.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public List<string> Genres { get; set; }
+        public int AgeLimit { get; set; } 
+        public int Rating { get; set; }
+        public List<Person> Actors { get; set; }
+        public Person Director { get; set; }    
+        public string Synopsis { get; set; }
+
+    }
+}
---
+++ MovieRepository/Services/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using MovieRepository.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRepository.Services
+{
+    public class MovieService
+    {
+        private readonly ILogger<MovieService> _logger;
+        public MovieService(ILogger<MovieService> logger) 
+        { 
+            _logger=  logger;
+        }   
+
+        public async Task<List<Movie>> GetAllMovies()
+        {
+            return new List<Movie>();
+        }
+
+        public async Task<Movie> GetById()
+        {
+            return new Movie();
+        }
+
+        public async Task<List<Movie>> Search(string searchTerm)
+        {
+            return new List<Movie>();
+        }
+    }
+}
